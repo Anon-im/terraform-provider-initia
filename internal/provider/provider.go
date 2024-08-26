9d5dcc98016b0045cce3fb815598f7a7372c092c
etienne-napoleone <etienne.napoleone@gmail.com>
2024-08-26 11:24:56 +0200

    chore: initial commit

---
+++ internal/provider/provider.go
@@ -0,0 +1,16 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{
+			"initia_tendermint_nodekey": resourceTendermintNodeKey(),
+		},
+	}
+}
---
+++ internal/provider/ressource_tendermint_nodekey.go
@@ -0,0 +1,88 @@
+package provider
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/tendermint/tendermint/crypto/ed25519"
+)
+
+type PrivKeyJSON struct {
+	Type  string `json:"type"`
+	Value string `json:"value"`
+}
+
+type NodeKeyJSON struct {
+	PrivKey PrivKeyJSON `json:"priv_key"`
+}
+
+func resourceTendermintNodeKey() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceTendermintNodeKeyCreate,
+		ReadContext:   resourceTendermintNodeKeyRead,
+		DeleteContext: resourceTendermintNodeKeyDelete,
+
+		Schema: map[string]*schema.Schema{
+			"node_id": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Tendermint node ID.",
+			},
+			"node_key": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Sensitive:   true,
+				Description: "Tendermint node key.",
+			},
+			"node_key_json": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Sensitive:   true,
+				Description: "Tendermint node key in the JSON format expected by a node.",
+			},
+		},
+	}
+}
+
+func resourceTendermintNodeKeyCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	privKey := ed25519.GenPrivKey()
+	pubKey := privKey.PubKey()
+
+	nodeId := strings.ToLower(pubKey.Address().String())
+	privKeyBase64 := base64.StdEncoding.EncodeToString(privKey.Bytes())
+
+	nodeKeyJSON := NodeKeyJSON{
+		PrivKey: PrivKeyJSON{
+			Type:  "tendermint/PrivKeyEd25519",
+			Value: privKeyBase64,
+		},
+	}
+
+	nodeKeyJSONString, err := json.Marshal(nodeKeyJSON)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.Set("node_id", nodeId)
+	d.Set("node_key", privKeyBase64)
+	d.Set("node_key_json", string(nodeKeyJSONString))
+
+	d.SetId(nodeId)
+
+	return diags
+}
+
+func resourceTendermintNodeKeyRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceTendermintNodeKeyDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	d.SetId("")
+	return nil
+}
